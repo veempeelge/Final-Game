ab76da510e91e58d3f333611a19d810d312e327f
Troosfey <156042400+Troosfey@users.noreply.github.com>
2024-06-02 17:31:45 +0700

    Spawn random weapon and change stats YAYYYY

--- Assets/Scripts/PP_Behave.cs
+++ Assets/Scripts/PP_Behave.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class PP_Behave : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            MovementPlayer1 mvP1 = other.GetComponent<MovementPlayer1>();
+
+            if (mvP1 != null)
+            {
+                if (gameObject.tag == "RollPin")
+                {
+                    mvP1.ChangeStats(3, 2, 5, 2, 6, 5);
+                }
+                else if (gameObject.tag == "MeatHam")
+                {
+                    mvP1.ChangeStats(5, 1, 6, 2, 10, 3);
+                }
+                else if (gameObject.tag == "BSpoon")
+                {
+                    mvP1.ChangeStats(2, 3, 5, 3, 7, 9);
+                }
+                else if (gameObject.tag == "Book")
+                {
+                    mvP1.ChangeStats(4, 4, 5, 5, 5, 9);
+                }
+
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.LogError("No MovementPlayer1 component found on the player object.");
+            }
         }
     }
 }
