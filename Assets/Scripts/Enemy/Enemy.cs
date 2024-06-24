0f176f689e9dc0507a6443031f8661f0ffb029a7
Nunew1 <nouvelnakano@gmail.com>
2024-06-24 15:13:06 +0700

    ChatGpt gak guna anjir

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
@@ -9,64 +8,73 @@ public class Enemy : MonoBehaviour
 {
     public static Enemy instance;
     public float rotationSpeed = 1f;
-    public float Speed;
+    public float speed;
     public float health;
     public float enemyAttack = 2;
     public float knockbackDuration = 0.2f;
-    Vector3 parameter = new Vector3 (1,0,1);
-    public float UpdateSpeed = 0.1f;
-    GameObject player;
-    Transform player1;
-    int index;
-    public Rigidbody body;
-    public Transform Player1;
+    public float updateSpeed = 0.1f;
+    public float stoppingDistance = 2.0f; // Set an appropriate stopping distance
 
-    private NavMeshAgent Agent;
-    public List<GameObject> Players = new List<GameObject>();
+    private NavMeshAgent agent;
+    private List<GameObject> players = new List<GameObject>();
     private Transform closestPlayer;
     private bool wasAttacked;
-    private Transform currentEnemy;
+    private bool isKnockedBack;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
-        Agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = stoppingDistance; // Set the stopping distance
 
         GameObject[] foundPlayers = GameObject.FindGameObjectsWithTag("Player");
-
-        foreach (GameObject player in foundPlayers)
-        {
-            Players.Add(player);
-        }
+        players.AddRange(foundPlayers);
 
         StartCoroutine(FollowClosestPlayer());
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (closestPlayer != null)
         {
+            float distanceToPlayer = Vector3.Distance(transform.position, closestPlayer.position);
+
+            if (distanceToPlayer > stoppingDistance)
+            {
+                agent.isStopped = false;
+                agent.SetDestination(closestPlayer.position);
+            }
+            else
+            {
+                agent.isStopped = true;
+            }
+
             Vector3 direction = (closestPlayer.position - transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
         }
-        else
-        {
-            FindClosestPlayer();
-        }
-
-        
     }
 
     private Transform FindClosestPlayer()
     {
-        Players.RemoveAll(player => player == null); 
-
         Transform nearestPlayer = null;
         float shortestDistance = Mathf.Infinity;
 
-        foreach (GameObject player in Players)
+        foreach (GameObject player in players)
         {
+            if (player == null) continue;
+
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             if (distanceToPlayer < shortestDistance)
             {
@@ -80,47 +88,45 @@ public class Enemy : MonoBehaviour
 
     private IEnumerator FollowClosestPlayer()
     {
-        WaitForSeconds Wait = new WaitForSeconds(UpdateSpeed);
+        WaitForSeconds wait = new WaitForSeconds(updateSpeed);
 
         while (true)
         {
             closestPlayer = FindClosestPlayer();
-            currentEnemy = closestPlayer;
 
-            if (currentEnemy != null)
+            if (closestPlayer != null)
             {
-                Agent.SetDestination(closestPlayer.position);
-               // ChangeTarget();
+                agent.SetDestination(closestPlayer.position);
             }
 
-            yield return Wait;
+            yield return wait;
         }
-
-       
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-       
         if (collision.tag == "Water")
         {
             Debug.Log("Got hit by holy water");
             Destroy(collision.gameObject);
-            //ChangeTarget();
+            ChangeTarget();
         }
     }
 
-    public void TakeDamage(float damageAmount,Vector3 knockbackDirection, float knockbackForce)
+    public void TakeDamage(float damageAmount, Vector3 knockbackDirection, float knockbackForce)
     {
         health -= damageAmount;
         Rigidbody rb = GetComponent<Rigidbody>();
-        Vector3 multiplier = new Vector3(0, -knockbackDirection.y, 0);
-
+        isKnockedBack = true;
         if (rb != null)
         {
-            StartCoroutine(Knockback(-knockbackDirection + multiplier, knockbackForce));
+            StartCoroutine(Knockback(knockbackDirection, knockbackForce));
         }
-        
+
+        if (health <= 0)
+        {
+            Die();
+        }
     }
 
     public void OnPlayerDetected(Transform playerTransform, MovementPlayer1 playerStats)
@@ -131,18 +137,16 @@ public class Enemy : MonoBehaviour
             wasAttacked = true;
             TakeDamage(playerStats.playerAtk, directionToPlayer, playerStats.playerKnockback);
             Invoke(nameof(CanTakeDamage), 0.2f);
-         //   Debug.Log("Hit");
         }
     }
 
-   void CanTakeDamage()
+    void CanTakeDamage()
     {
         wasAttacked = false;
     }
 
     private void Die()
     {
-        // Handle enemy death
         Destroy(gameObject);
     }
 
@@ -150,7 +154,7 @@ public class Enemy : MonoBehaviour
     {
         float elapsedTime = 0f;
         Vector3 startPosition = transform.position;
-        Vector3 targetPosition = startPosition + direction * knockbackForce;
+        Vector3 targetPosition = startPosition + direction * - knockbackForce;
 
         while (elapsedTime < knockbackDuration)
         {
@@ -159,28 +163,21 @@ public class Enemy : MonoBehaviour
             yield return null;
         }
 
-        if (health <= 0)
-        {
-            yield return new WaitForSeconds(.1f);
-            Die();
-        }
-
         transform.position = targetPosition; // Ensure the final position is set
     }
 
     public void ChangeTarget()
     {
-       // List playernya di randomize
+        // Randomize the list of players
+        if (players.Count > 0)
+        {
+            GameObject randomPlayer;
+            do
+            {
+                randomPlayer = players[Random.Range(0, players.Count)];
+            } while (randomPlayer.transform == closestPlayer);
 
-       // if result = currentEnemy
-       ChangeTarget();
-       // else 
-       // currentEnemy = hasilRandomizenya
-
-       //^^ Biar ga dpt enemy yg sama
-
-       // diatas lu cache playernya atau (udh gw lakuin, atau kalau mau ganti2 lagi)
-       // call ini method waktu kena collisionnya water (ud gw lakuin)
-       
+            closestPlayer = randomPlayer.transform;
+        }
     }
 }
